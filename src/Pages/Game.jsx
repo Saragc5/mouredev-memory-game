3f640dbc6cbc362362da9fdf4ee9aa1aa15e0b38
Saragc <sara255530@gmail.com>
2023-10-05 14:14:46 +0200

    add game page where it will be shown the game after the level choice

---
+++ src/Pages/Game.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Easy from '../Components/Easy';
+import Header from '../Components/Header';
+
+export default function Game() {
+  return (
+    <div>
+      <Header />
+      <Easy />
+    </div>
+  );
+}
