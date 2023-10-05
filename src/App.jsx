69ef9a7ce55dfd684641265c47109c5a74072961
Saragc <sara255530@gmail.com>
2023-10-05 14:11:55 +0200

    add routes for several level pages of the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
 import Easy from './Components/Easy';
-import Header from './Components/Header';
+import Medium from './Components/Medium';
+import Advanced from './Components/Advanced';
 
 import './App.scss';
 
 function App() {
   return (
     <main>
-      <Header />
-      <Easy />
+      <Routes>
+        <Route>
+          <Route index element={<Home />} />
+          <Route path="easy" element={<Easy />} />
+          <Route path="medium" element={<Medium />} />
+          <Route path="advanced" element={<Advanced />} />
+        </Route>
+      </Routes>
     </main>
   );
 }
