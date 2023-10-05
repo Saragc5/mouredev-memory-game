f4e04767bbbb707f0753a8a4d3e078f78f1f7e9a
Saragc <sara255530@gmail.com>
2023-10-05 19:37:51 +0200

    add button to choose level in Header only when the player is on a level, hidden when the player is in Home

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import './Header.scss';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function Header() {
   const [clicked, setClicked] = useState(false);
   const navigate = useNavigate();
+
+  const location = useLocation();
+  const isHome = location.pathname === "/";
 
   const handleChangeLevel = () => {
     if (!clicked) {
@@ -16,13 +19,22 @@ export default function Header() {
     <>
       <h1>Memory game</h1>
       <div className="options-game">
-        <span id="time-counter" name="time-counter" className="counter">Time counter</span>
+        <span id="time-counter" name="time-counter" className="counter">
+          Time counter
+        </span>
         <span className="counter">Move counter</span>
         <span className="counter">Remaining counter</span>
       </div>
-      <button className="btn-level" type="button" onClick={handleChangeLevel}>
-        Choose level
-      </button>
+      {isHome ? (
+        <button className="btn-level-hidden" type="button">
+          Choose level
+        </button>
+      ) : (
+        <button className="btn-level" type="button" onClick={handleChangeLevel}>
+          Choose level
+        </button>
+      )}
+
       <hr />
     </>
   );
