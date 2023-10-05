656657d2f3a0f2f63c42994f76121a5c3670a847
Saragc <sara255530@gmail.com>
2023-10-05 19:28:32 +0200

    add button to come back to level choices

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.scss';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
-  // const chooseLevel = () => {
+  const [clicked, setClicked] = useState(false);
+  const navigate = useNavigate();
 
-  // };
+  const handleChangeLevel = () => {
+    if (!clicked) {
+      setClicked(true);
+      navigate('/');
+    }
+  };
   return (
     <>
       <h1>Memory game</h1>
       <div className="options-game">
-        <span id="time-counter" name="time-counter" className="counter">
-          Time counter
-        </span>
-
+        <span id="time-counter" name="time-counter" className="counter">Time counter</span>
         <span className="counter">Move counter</span>
         <span className="counter">Remaining counter</span>
       </div>
-      <button className="btn-level" type="button">
+      <button className="btn-level" type="button" onClick={handleChangeLevel}>
         Choose level
       </button>
+      <hr />
     </>
   );
 }
